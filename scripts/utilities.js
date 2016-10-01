e0ae9d9821f4bbc95f86dd2c47d058c74007fb53
gsotorrio <willybosko@gmail.com>
2016-10-01 11:29:11 +0200

    Cleaning code

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -11,18 +11,24 @@
     if(month.toString().length == 1) {
         var month = '0'+month;
     }
+	
     if(day.toString().length == 1) {
         var day = '0'+day;
-    }   
+    }
+	
     if(hour.toString().length == 1) {
         var hour = '0'+hour;
+		
     }
+	
     if(minute.toString().length == 1) {
         var minute = '0'+minute;
     }
+	
     if(second.toString().length == 1) {
         var second = '0'+second;
-    }   
+    }
+	
     var dateTime = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second;   
      return dateTime;
 }
