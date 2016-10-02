d91afd2f4f8aac4439df998c2ca038978beaf734
gsotorrio <willybosko@gmail.com>
2016-10-02 15:44:11 +0200

    Trying use array with some change

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -8,42 +8,40 @@
     var hour    = now.getHours();
     var minute  = now.getMinutes();
     var second  = now.getSeconds();
-
+	var weekDay = now.getDay();	
 	var weekDay = new Array(7);
-		weekDay[0]=  "Sunday";
-		weekDay[1] = "Monday";
-		weekDay[2] = "Tuesday";
-		weekDay[3] = "Wednesday";
-		weekDay[4] = "Thursday";
-		weekDay[5] = "Friday";
-		weekDay[6] = "Saturday";
-	var weekDay = now.getDay;	
-	
-    if(month.toString().length == 1) {
-        var month = '0'+month;
+  		weekDay[0] = "Sunday";
+  		weekDay[1] = "Monday";
+		weekday[2] = "Tuesday";
+		weekday[3] = "Wendnesday";
+  		weekDay[4] = "Thursday";
+  		weekDay[5] = "Friday";
+  		weekDay[6] = "Saturday";
+
+    if (month.toString().length == 1) {
+			month = '0'+month;
     }
 	
-    if(day.toString().length == 1) {
-        var day = '0'+day;
+    if (day.toString().length == 1) {
+			day = '0'+day;
     }
 	
 	if (weekDay.toString().length == 1) {
-		var weekDay = '0'+weekDay;
+			weekDay = '0'+weekDay;
 	}
 	
-    if(hour.toString().length == 1) {
-        var hour = '0'+hour;
-		
+    if (hour.toString().length == 1) {
+			hour = '0'+hour;
     }
 	
-    if(minute.toString().length == 1) {
-        var minute = '0'+minute;
+    if (minute.toString().length == 1) {
+			minute = '0'+minute;
     }
 	
-    if(second.toString().length == 1) {
-        var second = '0'+second;
+    if (second.toString().length == 1) {
+			second = '0'+second;
     }
 	
-    var dateTime = year+'/'+month+'/'+day+' '+weekDay+' '+hour+':'+minute+':'+second;   
+    var dateTime = year+'/'+month+'/'+day+' '+"Today is "+weekDay+' '+"Current time is: "+hour+':'+minute+':'+second ;   
      return dateTime;
 }
