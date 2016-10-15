9551168262e648af0b70b229f1262de14be45719
osotorrio <osotorrio@hotmail.com>
2016-10-15 09:03:47 +0100

    Refactor

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,15 +1,15 @@
 "use strict";
 
 function getNameWeekDay(now){
-	var WeekDay = ["Monday", "Tuesday", "Wendnesday", "Thursday", "Friday", "Saturday",
-						"Sunday"];
+	var WeekDay = ["Monday", "Tuesday", "Wendnesday", "Thursday", "Friday", "Saturday", "Sunday"];
+	
 	return WeekDay[now.getDay()];					
 }
 
  function getDateTime() {
     var now     = new Date(); 
     var year    = now.getFullYear();
-    var month   = now.getMonth()+1; 
+    var month   = now.getMonth() + 1; 
     var day     = now.getDate();
     var hour    = now.getHours();
     var minute  = now.getMinutes();
@@ -41,8 +41,7 @@ function getNameWeekDay(now){
 			second = '0'+second;
     }
 	
-    var dateTime = year+'/'+month+'/'+day+' '+"Today is "+getNameWeekDay(now)+' '+"Current time is: "+hour+':'+minute+':'+second ;   
-     return dateTime;
+    return year + '/' + month + '/' + day + ' ' + "Today is " + getNameWeekDay(now) + ' ' + "Current time is: " + hour + ':' + minute + ':' + second;
 }
 
 function showDate(){
