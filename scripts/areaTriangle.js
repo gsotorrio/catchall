0edddd90f009a86ed66e0b282487c30bce99c27f
gsotorrio <willybosko@gmail.com>
2016-10-18 15:54:12 +0200

    I hava the equation

--- scripts/areaTriangle.js
+++ scripts/areaTriangle.js
@@ -1,2 +1,8 @@
 "use strict";
 
+var side1 = 0;   
+var side2 = 0;   
+var side3 = 0;
+   
+var perimeter = (side1 + side2 + side3)/2;  
+var area =  Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3)));  
