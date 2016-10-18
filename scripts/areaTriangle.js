4d962cff16b39676d9fad527385850c5a458a124
gsotorrio <willybosko@gmail.com>
2016-10-18 17:10:21 +0200

    get sides from html

--- scripts/areaTriangle.js
+++ scripts/areaTriangle.js
@@ -1,8 +1,8 @@
 "use strict";
 
-var side1 = 0;   
-var side2 = 0;   
-var side3 = 0;
+var side1 = window.document.getElementById("A").value;   
+var side2 = window.document.getElementById("B").value;   
+var side3 = window.document.getElementById("C").value;
    
 var perimeter = (side1 + side2 + side3)/2;  
 var area =  Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3)));  
