166d3afd8da6aeeaf0569c7c6bc7584bccccdd15
gsotorrio <willybosko@gmail.com>
2016-10-19 16:40:29 +0200

    Change and trying again

--- scripts/areaTriangle.js
+++ scripts/areaTriangle.js
@@ -1,23 +1,22 @@
 "use strict";
 
-var side1 = window.document.getElementById("A").value;   
-var side2 = window.document.getElementById("B").value;   
-var side3 = window.document.getElementById("C").value;
+var side1 = window.document.getElementById("A");   
+var side2 = window.document.getElementById("B");   
+var side3 = window.document.getElementById("C");
 
-function area (itemClicked){
+function area (side1, side2, side3){
 	
 	if(side1 || side2 || side3 == NaN){
 		alert ("This isnt a number");
+		if (side1 || side2 || side3 <= 0){
+			alert ("Please use positive numbers");
+		}
 	}
 	
-	else if (side1 || side2 || side3 <= 0){
-		alert ("Please use positive numbers");
-	}
-	
-	else (){
+	else {
 		var perimeter = (side1 + side2 + side3)/2;  
-		var opetation =  Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3))); 
-		return (operation);
+		var operation =  Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3))); 
+		alert (operation);
 	}
 }
 
