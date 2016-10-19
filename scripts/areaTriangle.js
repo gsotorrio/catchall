a1be032ab03b6e3dcaf6057ba05a331f7c1a8e9d
gsotorrio <willybosko@gmail.com>
2016-10-19 15:51:32 +0200

    Making the function

--- scripts/areaTriangle.js
+++ scripts/areaTriangle.js
@@ -3,6 +3,20 @@
 var side1 = window.document.getElementById("A").value;   
 var side2 = window.document.getElementById("B").value;   
 var side3 = window.document.getElementById("C").value;
+
+function area (side1, siede2, side3){
+	
+	if(side1 || side2 || side3 == NaN){
+		alert ("This isnt a number");
+	}
+	
+	else if (side1 || side2 || side3 <= 0){
+		alert ("Please use positive numbers");
+	}
+	
+	else (){}
+}
+
    
 var perimeter = (side1 + side2 + side3)/2;  
 var area =  Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3)));  
