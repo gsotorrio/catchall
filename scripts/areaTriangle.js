14d15499248c5bac6d932a432838e62ad234ae82
gsotorrio <willybosko@gmail.com>
2016-10-21 15:08:28 +0200

    else if

--- scripts/areaTriangle.js
+++ scripts/areaTriangle.js
@@ -8,16 +8,18 @@ function area (side1, side2, side3){
 	
 	if(side1 || side2 || side3 == NaN){
 		alert ("This isnt a number");
-		if (side1 || side2 || side3 <= 0){
+	}
+		
+	else if (side1 || side2 || side3 <= 0){
 			alert ("Please use positive numbers");
-		}
 	}
 	
 	else {
 		var perimeter = (side1 + side2 + side3)/2;  
 		var operation =  Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3))); 
-		alert (operation);
 	}
+	
+	alert (operation);
 }
 
    
