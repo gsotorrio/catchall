aabf5e86fc69ae5e1d845a1236f1d87d9ca65a24
gsotorrio <willybosko@gmail.com>
2016-10-24 10:16:59 +0200

    cleaning HTML and correcting js

--- scripts/areaTriangle.js
+++ scripts/areaTriangle.js
@@ -1,25 +1,23 @@
 "use strict";
 
-var side1 = window.document.getElementById("A").value;   
-var side2 = window.document.getElementById("B").value;   
-var side3 = window.document.getElementById("C").value;
-
-function area (side1, side2, side3){
+function area (){
 	
-	if(side1 || side2 || side3 == NaN){
+	var side1 = window.document.getElementById("A").value;   
+	var side2 = window.document.getElementById("B").value;   
+	var side3 = window.document.getElementById("C").value;
+	
+	if(side1 == NaN || side2 == NaN || side3 == NaN){
 		alert ("This isnt a number");
 	}
 		
-	else if (side1 || side2 || side3 <= 0){
+	if (side1 <= 0 || side2 <= 0 || side3 <= 0){
 			alert ("Please use positive numbers");
 	}
-	
 	else {
 		var perimeter = (side1 + side2 + side3)/2;  
-		var operation =  Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3))); 
-	}
-	
-	alert (operation);
+		var result =  Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3))); 
+		window.document.getElementById("result").innerText = result;
+	}	
 }
 
    
