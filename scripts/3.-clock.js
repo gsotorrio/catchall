354c8950468551e98183cbe779783b98395c8067
gsotorrio <willybosko@gmail.com>
2016-10-25 18:54:43 +0200

    get the time

---
+++ scripts/3.-clock.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var myVar = setInterval(function(){ myTimer() }, 1000);
+
+function myTimer() {
+    var d = new Date();
+    var t = d.toLocaleTimeString();
+    document.getElementById("demo").innerHTML = t;
+}
