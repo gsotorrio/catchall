bd5b6c2f80b126e99a6a581e25aa6f790f3ab4fc
gsotorrio <willybosko@gmail.com>
2016-11-04 07:14:59 +0100

    Clean code and comment

--- scripts/4.-age.js
+++ scripts/4.-age.js
@@ -2,26 +2,31 @@
 var totalBorn ="";
 var totalToday = "";
 
+// Function for get totalBorn and totalToday
 function takeDates(){
 	var giveDate = window.document.getElementById("textBox").value;
-	
+
+	// Take the user born date and convert this date in days	
 	var bornDate = new Date (giveDate);
 	var bornYear = bornDate.getYear();
 	var bornMounth = bornDate.getMonth()+1;
 	var bornDay = bornDate.getDate();
 	totalBorn = (bornYear+1900)*365 + bornMounth*30 + bornDay;
-	
+
+	// Conditional for know if you born before 1900	
 	if (bornYear < 0){
 		totalBorn = "";
 		alert ("You are died");
 	}
-	
+
+	// Convert in days today date	
 	var todayDate = new Date();
 	var year = todayDate.getFullYear();
 	var mounth = todayDate.getMonth()+1;
 	var day = todayDate.getDate();
 	totalToday = year*365 + mounth*30 + day;
-	
+
+	// Conditional check if you put well the date 	
 	if (isNaN(bornMounth) == true){
 		alert ("Please use a correct date");
 		totalBorn = "";
@@ -29,10 +34,15 @@ function takeDates(){
 	}
 }	
 
+// This function get the solution.
 function operate(){
+
+	// var assistant for calculate	
 	var total = totalToday - totalBorn;
 	var a = total/365;
 	var b = "";
+
+	// var for return the solution	
 	var year = Math.floor(a)
 	var mounth = 0;
 	var days = 0;
@@ -60,7 +70,8 @@ function operate(){
 		}	
 }
 
+// Function for show the solution.
 function giveTimeYourLive (){
-	window.document,getElementById("age").value = operte();
+	window.document.getElementById("age").innerText = operate();
 }
 
