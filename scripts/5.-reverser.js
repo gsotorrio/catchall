72e3b5e3769d2662d9f5fe1eabc9673627e5c9d3
gsotorrio <willybosko@gmail.com>
2016-11-04 11:11:43 +0100

    Comentaries

--- scripts/5.-reverser.js
+++ scripts/5.-reverser.js
@@ -1,15 +1,22 @@
 "use strict";
-
+// Function do the reserver
 function reverser(){
+	// This var take the number that the user enter
 	var number = window.document.getElementById("numberBox").value;
+	
+	// This is asistant var for know if is a number.
 	var aux = parseInt(window.document.getElementById("numberBox").value);
+	
+	// Asistants var for reverser the number 
 	var x = number.length;
     var numberRverser = "";
  
+	// Conditional for know if the user give numbers or no.
 	if (isNaN(aux) == true){
 		alert ("please only numbers")
 	}
 	else{
+		// This make the reverser.
 		while (x>=0) {
 			numberRverser = numberRverser + number.charAt(x);
 			x--;
@@ -18,6 +25,7 @@ function reverser(){
   return "Your new number is: " +  numberRverser;
 }
 
+// Function show the new number.
 function showNumberReverser(){
 	window.document.getElementById("numRev").innerText = reverser();
 }
