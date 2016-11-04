930034a4c4d5519cea3ba2e2ac792a010546da3d
gsotorrio <willybosko@gmail.com>
2016-11-04 10:59:40 +0100

    Function for show the solution and conditional

--- scripts/5.-reverser.js
+++ scripts/5.-reverser.js
@@ -2,14 +2,22 @@
 
 function reverser(){
 	var number = window.document.getElementById("numberBox").value;
+	var aux = parseInt(window.document.getElementById("numberBox").value);
 	var x = number.length;
     var numberRverser = "";
  
-  while (x>=0) {
-    numberRverser = numberRverser + number.charAt(x);
-    x--;
-  }
-  return numberRverser;
-
+	if (isNaN(aux) == true){
+		alert ("please only numbers")
+	}
+	else{
+		while (x>=0) {
+			numberRverser = numberRverser + number.charAt(x);
+			x--;
+		}
+	}	
+  return "Your new number is: " +  numberRverser;
 }
 
+function showNumberReverser(){
+	window.document.getElementById("numRev").innerText = reverser();
+}
