aaefc20d886cdae86a7193c8f7c28778ceb9d179
gsotorrio <willybosko@gmail.com>
2016-11-05 12:19:19 +0100

    Refactor exercise 4.-age

--- scripts/4.-age.js
+++ scripts/4.-age.js
@@ -9,9 +9,9 @@ function takeDates(){
 	// Take the user born date and convert this date in days	
 	var bornDate = new Date (giveDate);
 	var bornYear = bornDate.getYear();
-	var bornMounth = bornDate.getMonth()+1;
+	var bornMounth = bornDate.getMonth() + 1;
 	var bornDay = bornDate.getDate();
-	totalBorn = (bornYear+1900)*365 + bornMounth*30 + bornDay;
+	totalBorn = (bornYear + 1900)*365 + bornMounth*30 + bornDay;
 
 	// Conditional for know if you born before 1900	
 	if (bornYear < 0){
@@ -22,7 +22,7 @@ function takeDates(){
 	// Convert in days today date	
 	var todayDate = new Date();
 	var year = todayDate.getFullYear();
-	var mounth = todayDate.getMonth()+1;
+	var mounth = todayDate.getMonth() + 1;
 	var day = todayDate.getDate();
 	totalToday = year*365 + mounth*30 + day;
 
@@ -32,6 +32,8 @@ function takeDates(){
 		totalBorn = "";
 		totalToday="";
 	}
+	
+	operate();
 }
 
 // Function get hours and minutes
@@ -73,15 +75,9 @@ function operate(){
 	
 	days = b - mounth*30;
 	
-	if (totalToday == ""){
+	if (totalToday != ""){
+		window.document.getElementById("age").innerText = "You are living" + " " + year + "years," + " " + mounth + "mounths," + " " + days + "days" + " " + "and this time " + getTime();
 	}
-	else {
-		return "You are living" + " " + year + "years," + " " + mounth + "mounths," + " " + days + "days" + " " + "and this time " + getTime();
-		}	
 }
 
-// Function for show the solution.
-function showTimeYourLive (){
-	window.document.getElementById("age").innerText = operate();
-}
 
