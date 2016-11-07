13255dda224c3eaaadbca3e0264ebce4e3ae21c5
gsotorrio <willybosko@gmail.com>
2016-11-07 11:09:49 +0100

    Get reverser array

--- scripts/8.-reversesArray.js
+++ scripts/8.-reversesArray.js
@@ -1,1 +1,6 @@
 "use strict";
+
+function reverseArray(){
+	var originalArray = [1, 40, 3, 20, 5, 13, 7, 10, 15];
+	window.document.getElementById("newArray").innerText = originalArray.reverse();
+}
