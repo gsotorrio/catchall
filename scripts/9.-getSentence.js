a55ef1c573e38d46369ac2e4e9096e6d78d38a81
gsotorrio <willybosko@gmail.com>
2016-11-12 13:10:13 +0100

    finish get sentence

--- scripts/9.-getSentence.js
+++ scripts/9.-getSentence.js
@@ -2,14 +2,18 @@
 
 function getSentence(){
 	var mySentence = [["Mi", "nombre"], ["es", "Guillermo"], ["Sotorrio", "y"], ["tengo", "30"], ["años", "creo!!"]]; 
+	var getWordsFor = "";
+	var saveWordsAndWriteSentence = "";
 	
 	for(var i = 0; i < mySentence.length; i++){
 		
 		var secondElementArray = mySentence[i];
 		
 		for(var j = 0; j < secondElementArray.length; j++){
-		console.log(mySentence[i][j]);
-		window.document.getElementById("solution").innerText = mySentence[i][j];
+		getWordsFor = mySentence[i][j];
+		saveWordsAndWriteSentence = saveWordsAndWriteSentence + " " + getWordsFor;	
 		}
 	}
+	
+	window.document.getElementById("solution").innerText = saveWordsAndWriteSentence;
 }
