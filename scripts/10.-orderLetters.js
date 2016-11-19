f06ff318b01305ffed6d3263ad35661eb66012b3
gsotorrio <willybosko@gmail.com>
2016-11-19 07:45:25 +0100

    Function is working

--- scripts/10.-orderLetters.js
+++ scripts/10.-orderLetters.js
@@ -1,2 +1,8 @@
 "use strict";
 
+function putlettersInCorrectOrder(){
+    var getLettersForUser = window.document.getElementById("letterbox").value;
+    console.log(getLettersForUser);
+
+    window.document.getElementById("showOrderLetters").innerText = "The alphabetical order is:" + " " + getLettersForUser.split('').sort().join('');
+}
