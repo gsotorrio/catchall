d7fe085fc74bdab4d13cea6ec13b22076c352a33
gsotorrio <willybosko@gmail.com>
2016-11-20 20:35:08 +0100

    convert string in array

---
+++ scripts/11.-randomCharacteres.js
@@ -0,0 +1,5 @@
+function showAllCharacteresUserWant(){
+    var characteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var arrayCharacteres = characteres.split("");
+    console.log(arrayCharacteres);
+}
