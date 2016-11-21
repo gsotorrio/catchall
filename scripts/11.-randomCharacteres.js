2e5d700dd831ffff37a40f877077746ae78112a7
gsotorrio <willybosko@gmail.com>
2016-11-21 20:31:00 +0100

    shave changes

--- scripts/11.-randomCharacteres.js
+++ scripts/11.-randomCharacteres.js
@@ -1,24 +1,21 @@
-function showAllCharacteresUserWant(count){
+function showAllCharacteresUserWant(){
     var characteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     var arrayCharacteres = characteres.split("");
-    var slectElements = arrayCharacteres.slice(arrayCharacteres);
     var showTheResult = [];
 
-    count = parseInt(window.document.getElementById("numberCharacteres").value);
+    var count = parseInt(window.document.getElementById("numberCharacteres").value);
  
     if (isNaN(count) == true){
         window.document.getElementById("divError").style.display = "inline-block";
     }
-
     else{
         for (var i = 0; i < count; i++){
-            window.document.getElementById("divError").style.display = "none";
-
-            var formulaForGetRamdomElements = Math.floor(Math.random() * slectElements.length);
-            var newArrayWithRamdomElements = slectElements.splice(formulaForGetRamdomElements, 1);
+            var formulaForGetRamdomElements = Math.floor(Math.random() * arrayCharacteres.length);
+            var newArrayWithRamdomElements = arrayCharacteres.splice(formulaForGetRamdomElements, 1);
         
             showTheResult.push(newArrayWithRamdomElements[0]);
         }
+        window.document.getElementById("divError").style.display = "none";
         window.document.getElementById("showCharacteres").innerText = "These are your characteres:" + " " + "[" + showTheResult + "]";  
     }
 }
