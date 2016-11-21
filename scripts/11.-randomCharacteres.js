1550c42ee20f407cde6b4fbf0ca3d96b7f6f5904
gsotorrio <willybosko@gmail.com>
2016-11-21 04:33:14 +0100

    exercise 11 ramdom characteres work good

--- scripts/11.-randomCharacteres.js
+++ scripts/11.-randomCharacteres.js
@@ -1,5 +1,26 @@
-function showAllCharacteresUserWant(){
+function showAllCharacteresUserWant(count){
     var characteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     var arrayCharacteres = characteres.split("");
-    console.log(arrayCharacteres);
+    var slectElements = arrayCharacteres.slice(arrayCharacteres);
+    var showTheResult = [];
+
+    count = parseInt(window.document.getElementById("numberCharacteres").value);
+ 
+    if (isNaN(count) == true){
+        //var a = "melon solo numeros";
+        //alert(a);
+        window.document.getElementById("divError").style.display = "inline-block";
+    }
+
+    else{
+        for (var i = 0; i < count; i++){
+            window.document.getElementById("divError").style.display = "none";
+            
+            var formulaForGetRamdomElements = Math.floor(Math.random() * slectElements.length);
+            var newArrayWithRamdomElements = slectElements.splice(formulaForGetRamdomElements, 1);
+        
+            showTheResult.push(newArrayWithRamdomElements[0]);
+        }
+        window.document.getElementById("showCharacteres").innerText = "These are your characteres:" + " " + "[" + showTheResult + "]";  
+    }
 }
