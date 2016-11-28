85752066879993acb21ef5808794ab8bc124a3b2
gsotorrio <willybosko@gmail.com>
2016-11-28 08:50:18 +0100

    exercises 12

---
+++ scripts/12.-longerString.js
@@ -0,0 +1,15 @@
+function showTheLongerString (){
+    var getOriginalString = window.document.getElementById("originalString").innerText;
+    var stringInArray = getOriginalString.split(" ");
+    var longString = "";
+    var longerArray = "";
+
+    for(var i = 0; i < stringInArray.length; i++){
+
+        if (stringInArray[i].length > longString){
+            longString = stringInArray[i].length;
+            longerArray = stringInArray[i];
+        }
+    }
+    window.document.getElementById("stringWin").innerText = "In this sentence the word more long is:" + " " + longerArray;
+}
