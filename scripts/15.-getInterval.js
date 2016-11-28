0c81424251557afe0a1c83ad6a4dd7cabd67aad3
gsotorrio <willybosko@gmail.com>
2016-11-28 09:56:43 +0100

    exercise 15

--- scripts/15.-getInterval.js
+++ scripts/15.-getInterval.js
@@ -1,11 +1,16 @@
 function getInterval (){
     var firstNumberInterval = window.document.getElementById("firstNumber").value;
     var secondNumberInterval = window.document.getElementById("secondNumber").value;
-    
     var arrayNumbers = [];
 
-    for (var i = 0; i <100;  i++){
-        arrayNumbers.splice(i, 0, i);
+    if (firstNumberInterval < 0 || secondNumberInterval > 100){
+         window.document.getElementById("textError").style.display = "inline-block";
+    }
+    else {
+        for (var i = 0; i <100;  i++){
+            arrayNumbers.splice(i, 0, i);
+        }
+        window.document.getElementById("textError").style.display = "none";    
     }
     window.document.getElementById("interval").innerText = arrayNumbers.slice(firstNumberInterval, secondNumberInterval);
 }
