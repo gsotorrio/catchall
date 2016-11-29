6fb8cba4a89a065ba8923525c0d2eaeeab833a41
gsotorrio <willybosko@gmail.com>
2016-11-29 09:02:09 +0100

    Changes in exercise 15

--- scripts/15.-getInterval.js
+++ scripts/15.-getInterval.js
@@ -1,7 +1,14 @@
 function getInterval (){
     var firstNumberInterval = window.document.getElementById("firstNumber").value;
     var secondNumberInterval = window.document.getElementById("secondNumber").value;
+    var aux;
     var arrayNumbers = [];
+
+    if (firstNumberInterval > secondNumberInterval ){
+         aux = firstNumberInterval
+        firstNumberInterval = secondNumberInterval;
+        secondNumberInterval = aux;
+    }
 
     if (firstNumberInterval < 0 || secondNumberInterval > 100){
          window.document.getElementById("textError").style.display = "inline-block";
