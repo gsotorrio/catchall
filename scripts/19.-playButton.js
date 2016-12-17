178d61ebfc7ae891b903cb1f506c7aa57fab256f
gsotorrio <willybosko@gmail.com>
2016-12-17 20:00:00 +0100

    take off the number 10000

--- scripts/19.-playButton.js
+++ scripts/19.-playButton.js
@@ -1,19 +1,15 @@
 "use strict";
 
 function changePositionbutton() {
-    var button = document.getElementById('myButton');
-    var postX = Math.floor(Math.random() *10000);
-    var postY = Math.floor(Math.random() *10000);
-    var widthScreen = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
-    var heightScreen = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
-   
-    if(postX > widthScreen || postY > heightScreen){
-        changePositionbutton();
-    }
-    else{
-        button.style.position = "absolute";
-        button.style.left = postY +'px';
-        button.style.top = postX +'px';
-    }
-}
+    var button = window.document.getElementById('myButton');
 
+    var widthScreen = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var heightScreen = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+   
+    var postX = Math.floor(Math.random() * widthScreen);
+    var postY = Math.floor(Math.random() * heightScreen);
+    
+    button.style.position = "absolute";
+    button.style.left = postY +'px';
+    button.style.top = postX +'px';
+}
